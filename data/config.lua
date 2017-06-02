3f79bec80d48f697aed6af0aec721279a1384056
xmaxman <hsam4156@gmail.com>
2017-06-02 13:16:01 +0430

    Add files via upload

---
+++ data/config.lua
@@ -0,0 +1,15 @@
+do local _ = {
+  enabled_plugins = {
+    "ping",
+    "plugins",
+    "typing",
+    "tabchi"
+  },
+  sudo_users = {
+    350419590,
+    123456789,
+    987654321
+  }
+}
+return _
+end
