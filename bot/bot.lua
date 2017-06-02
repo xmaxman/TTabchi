b2244e57a4e360ab0fa9cab5bce729946f851ea1
xmaxman <hsam4156@gmail.com>
2017-06-02 13:13:48 +0430

    Update bot.lua

--- bot/bot.lua
+++ bot/bot.lua
@@ -232,7 +232,7 @@ function create_config( )
 	"plugins",
     "tabchi"
     },
-    sudo_users = {12345678},--Sudo users
+    sudo_users = {315017219},--Sudo users
   }
   serialize_to_file(config, './data/config.lua')
   print('saved config into ./data/config.lua')
